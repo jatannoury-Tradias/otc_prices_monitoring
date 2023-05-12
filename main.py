0a6a5caa3ac3063a0cc57d9005f11d2387bf48a1
tradias-georgio-nehme <g.nehme.ext@bankhaus-scheich.de>
2023-05-12 22:25:48 +0300

    small adjustments

--- main.py
+++ main.py
@@ -1,66 +1,76 @@
 import asyncio
+import orjson
+import sys
+
 import websockets
-import json
-from websocket_helper import WebsocketHelper
 
-from config import ODIN_MONITOR
-
+from config import ODIN_MONITOR, MARKET_DATA_TOKEN
 from s3_controller import upload_to_s3
 
+ARGUMENTS = sys.argv
+
 MY_TOKEN = ODIN_MONITOR
-ENVIRONMENT = "otcapp"
-PROTOCOL = "wss"
 
-URI = f"{PROTOCOL}://{ENVIRONMENT}.tradias.de/otc/monitor/ws"
+MONITOR_URI = "wss://otcapp.tradias.de/otc/monitor/ws"
 
-async def get_prices(instrument: str) -> None:
-    memory_array=[]
-    memory_array_to_be_printed=[]
+headers = {
+    'x-token-id': MY_TOKEN
+}
 
-    websocket_connector = WebsocketHelper(url=f'{URI}?x-token-id={MY_TOKEN}')
-    await websocket_connector.keep_open(
-        extra_messages=[{
-            "type": "subscribe",
-            "channelname": "talosprices",
-            "instrument": instrument,
-            "heartbeat": True
-        }],
-        print_response=True)
 
-    # print(curr_res)
-    # if "levels" not in curr_res:
-    #     continue
-    # if curr_res['event'] != "Talos_All":
-    #     continue
-    # if len(memory_array) == 0:
-    #     memory_array.append(curr_res)
-    #
-    # elif len(memory_array_to_be_printed)!=0:
-    #     memory_array_to_be_printed.append(curr_res)
-    #     upload_to_s3(memory_array_to_be_printed)
-    #     memory_array_to_be_printed=[]
-    #     memory_array = []
-    # else:
-    #     stream_minus_1 = memory_array[-1]
-    #     for index,level in enumerate(curr_res['levels']["buy"]):
-    #         new_price = level["price"]
-    #         old_price = stream_minus_1['levels']['buy'][index]['price']
-    #         if abs(new_price - old_price) * 10_000 / (0.5 * (new_price+old_price)) > 0.000001:
-    #             memory_array_to_be_printed.append(stream_minus_1)
-    #             memory_array_to_be_printed.append(curr_res)
-    #             break
-    #     if len(memory_array_to_be_printed) != 0:
-    #         continue
-    #     for index,level in enumerate(curr_res['levels']["sell"]):
-    #         new_price = level["price"]
-    #         old_price = stream_minus_1['levels']['sell'][index]['price']
-    #         if abs(new_price - old_price) * 10_000 / (0.5 * (new_price+old_price)) > 0.000001:
-    #             memory_array_to_be_printed.append(stream_minus_1)
-    #             memory_array_to_be_printed.append(curr_res)
-    #             break
-async def main():
-    task_1 = asyncio.create_task(get_prices(instrument='BTCEUR'))
-    task_2 = asyncio.create_task(get_prices(instrument='ETHEUR'))
+async def get_prices(price_channel, instrument: str) -> None:
+    memory_array = []
+    memory_array_to_be_printed = []
+    async with websockets.connect(uri=MONITOR_URI, extra_headers=headers) as websocket:
+        await websocket.send(orjson.dumps({"type": "subscribe",
+                                         "channelname": price_channel,
+                                         "instrument": instrument,
+                                         "heartbeat": True}))
+        while True:
+            curr_res = orjson.loads(await websocket.recv())
+            print(curr_res)
+            if "levels" not in curr_res:
+                continue
+            if curr_res['event'] != "Talos_All":
+                continue
+            if len(memory_array) == 0:
+                memory_array.append(curr_res)
+
+            elif len(memory_array_to_be_printed) != 0:
+                memory_array_to_be_printed.append(curr_res)
+                upload_to_s3(memory_array_to_be_printed, price_channel)
+                memory_array_to_be_printed = []
+                memory_array = []
+            else:
+                stream_minus_1 = memory_array[-1]
+                for index, level in enumerate(curr_res['levels']["buy"]):
+                    new_price = level["price"]
+                    old_price = stream_minus_1['levels']['buy'][index]['price']
+                    if abs(new_price - old_price) * 10_000 / (0.5 * (new_price + old_price)) > 20:
+                        memory_array_to_be_printed.append(stream_minus_1)
+                        memory_array_to_be_printed.append(curr_res)
+                        break
+                if len(memory_array_to_be_printed) != 0:
+                    continue
+                for index, level in enumerate(curr_res['levels']["sell"]):
+                    new_price = level["price"]
+                    old_price = stream_minus_1['levels']['sell'][index]['price']
+                    if abs(new_price - old_price) * 10_000 / (0.5 * (new_price + old_price)) > 20:
+                        memory_array_to_be_printed.append(stream_minus_1)
+                        memory_array_to_be_printed.append(curr_res)
+                        break
+
+
+async def main(price_channel: str):
+    task_1 = asyncio.create_task(get_prices(instrument='BTCEUR', price_channel=price_channel))
+    task_2 = asyncio.create_task(get_prices(instrument='ETHEUR', price_channel=price_channel))
     await asyncio.gather(task_1, task_2)
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+    if ARGUMENTS[1] == 'talos':
+        print("Starting on Talos")
+        asyncio.run(main(price_channel='talosprices'))
+    else:
+        print('Starting on Prices')
+        asyncio.run(main(price_channel='prices'))
